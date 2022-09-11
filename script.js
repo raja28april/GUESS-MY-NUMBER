9d7a19e912a09c2a02182e3256762dd7cf4d33cb
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-11 20:46:29 +0200

    Raja: implementing high score logic

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 
 let secretNumber = Math.trunc((Math.random()*20)+1);
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -30,6 +31,10 @@ document.querySelector('.check').addEventListener('click',function(){
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor='#60b347';
         document.querySelector('.number').style.width='30rem';
+        if(score>highScore){
+            highScore=score;
+            document.querySelector('.highscore').textContent=highScore;
+        }
     
     //when guess is high
     }else if(guess>secretNumber){
