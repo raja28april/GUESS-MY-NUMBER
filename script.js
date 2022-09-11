64f3a572c1a87f46f4380683ba6417af14653662
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-11 20:37:59 +0200

    Raja: implementing logic for again button

--- script.js
+++ script.js
@@ -9,8 +9,7 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc((Math.random()*20)+1);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc((Math.random()*20)+1);
 let score = 20;
 
 document.querySelector('.check').addEventListener('click',function(){
@@ -28,6 +27,7 @@ document.querySelector('.check').addEventListener('click',function(){
     //when wins    
     }else if(guess===secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct number';
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor='#60b347';
         document.querySelector('.number').style.width='30rem';
     
@@ -53,4 +53,15 @@ document.querySelector('.check').addEventListener('click',function(){
             document.querySelector('.score').textContent =0;
         }
     }
-})
+});
+
+document.querySelector('.btn').addEventListener('click',function(){
+    score =20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent ='?';
+    document.querySelector('.guess').value ='';
+    document.querySelector('body').style.backgroundColor='#422';
+    document.querySelector('.number').style.width='15rem';
+    secretNumber = Math.trunc((Math.random()*20)+1);
+
+});
